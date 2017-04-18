65788da56aa14405c9dd26cd5ef0777e92f22473
leblanc119 <leblanc119@gmail.com>
2017-04-17 22:31:34 -0500

    Coords and Street to Weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -10,23 +10,26 @@ class ForecastController < ApplicationController
     @lat = params[:user_latitude]
     @lng = params[:user_longitude]
 
-    # ==========================================================================
-    # Your code goes below.
-    # The latitude the user input is in the string @lat.
-    # The longitude the user input is in the string @lng.
-    # ==========================================================================
+    require 'open-uri'
 
+    @url="https://api.darksky.net/forecast/6211586540cf1cda8f9a0dc4dc50db9b/"+@lat+","+@lng
 
+    parsed_data = JSON.parse(open(@url).read)
+    @current_temperature = parsed_data["currently"]["temperature"]
+    @current_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @next_hour = parsed_data["hourly"]["data"][0]["summary"]
+    @next_days = parsed_data["daily"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = @current_temperature
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = @current_summary
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = @next_hour
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = @hourly_summary
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = @next_days
 
     render("forecast/coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -9,23 +9,31 @@ class MeteorologistController < ApplicationController
   def street_to_weather
     @street_address = params[:user_street_address]
 
-    # ==========================================================================
-    # Your code goes below.
-    #
-    # The street address that the user typed is in the variable @street_address.
-    # ==========================================================================
+    require 'open-uri'
+    @url1="http://maps.googleapis.com/maps/api/geocode/json?address="+@street_address
 
+    parsed_data = JSON.parse(open(@url1).read)
+    @lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
+    @url2="https://api.darksky.net/forecast/6211586540cf1cda8f9a0dc4dc50db9b/"+@lat.to_s+","+@lng.to_s
 
-    @current_temperature = "Replace this string with your answer."
+    parsed_data = JSON.parse(open(@url2).read)
+    @current_temperature = parsed_data["currently"]["temperature"]
+    @current_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @next_hour = parsed_data["hourly"]["data"][0]["summary"]
+    @next_days = parsed_data["daily"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_temperature = @current_temperature
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @current_summary = @current_summary
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = @hourly_summary
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_hours = @next_hour
+
+    @summary_of_next_several_days = @next_days
 
     render("meteorologist/street_to_weather.html.erb")
   end
